fc43579de46b497b3a3b2d41c92ada419fb2869b
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-09 21:29:46 +0530

    Remove navigation bar and add styling

--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -1,11 +1,8 @@
-import { NavBar } from "../components/NavBar"
-
 export const Company = () => {
     return (
         <div>
-            <NavBar/>
-            <h2 className="text-center py-20 text-black text-4xl font-bold">
-                This is Company Page
+            <h2 className="text-center py-20 text-black text-4xl font-bold bg-slate-400 mt-10">
+                This is Company Sub Page
             </h2>
         </div>
     )
