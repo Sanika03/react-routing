633e4d4dcd1ba78447a1528e17ed633b5491200b
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-09 21:11:52 +0530

    Add navigation bar

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,6 +1,9 @@
+import { NavBar } from "../components/NavBar"
+
 export const Team = () => {
     return (
         <div>
+            <NavBar/>
             <h2 className="text-center py-20 text-black text-4xl font-bold">
                 This is Team Page
             </h2>
