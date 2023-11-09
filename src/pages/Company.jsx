aece5a7c9c2e933ad70fbae471ada0a432e7cb74
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-09 21:38:47 +0530

    Display company/team ID in link on screen.

--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -1,8 +1,11 @@
+import { useParams } from "react-router-dom"
+
 export const Company = () => {
+    const { companyId } = useParams();
     return (
         <div>
             <h2 className="text-center py-20 text-black text-4xl font-bold bg-slate-400 mt-10">
-                This is Company Sub Page
+                This is Company Sub Page with id as <span className="text-orange-700 underline">{companyId}</span>.
             </h2>
         </div>
     )
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,11 +1,14 @@
+import { useParams } from "react-router-dom"
 import { NavBar } from "../components/NavBar"
 
 export const Team = () => {
+    const { teamId } = useParams();
+
     return (
         <div>
             <NavBar/>
             <h2 className="text-center py-20 text-black text-4xl font-bold bg-slate-400 mt-10">
-                This is Team Sub Page
+                This is Team Sub Page with id as <span className="text-orange-700 underline">{teamId}</span>.
             </h2>
         </div>
     )
