dfc463405855e767f0c47639688afdf599d805aa
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-09 22:24:31 +0530

    Wrap profile route in ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/profile",
-    element: <Profile/>
+    element: <ProtectedRoute><Profile/></ProtectedRoute>
   },
   {
     path: "/about/team/:teamId",
