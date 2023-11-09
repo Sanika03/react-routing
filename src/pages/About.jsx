e99b94015b8fd5a35d65c7a8aaaffdb916db36b9
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-09 18:23:06 +0530

    Add 'About' page

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import { NavBar } from "../components/NavBar"
+
+export const About = () => {
+    return (
+        <div>
+            <NavBar/>
+            <h2 className="text-center py-20 text-black text-4xl font-bold">
+                This is About Page
+            </h2>
+        </div>
+    )
+}
