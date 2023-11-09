ddea02fd59cc771e99f85e70e3cce4797961350b
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-09 21:09:28 +0530

    Add 'Team' page

---
+++ src/pages/Team.jsx
@@ -0,0 +1,9 @@
+export const Team = () => {
+    return (
+        <div>
+            <h2 className="text-center py-20 text-black text-4xl font-bold">
+                This is Team Page
+            </h2>
+        </div>
+    )
+}
