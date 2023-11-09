c34842b97701db5d0155b89023449a18286cc367
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-09 21:29:57 +0530

    Add styling

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -4,8 +4,8 @@ export const Team = () => {
     return (
         <div>
             <NavBar/>
-            <h2 className="text-center py-20 text-black text-4xl font-bold">
-                This is Team Page
+            <h2 className="text-center py-20 text-black text-4xl font-bold bg-slate-400 mt-10">
+                This is Team Sub Page
             </h2>
         </div>
     )
