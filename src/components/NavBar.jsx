ff304ff9ee7250a23f8ca99c6cb06ea2c2f9f8c6
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-09 22:22:31 +0530

    Add underline styling to active link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,15 @@ import { NavLink } from "react-router-dom"
 export const NavBar = () => {
     return (
         <nav className="flex gap-4 justify-around flex-wrap bg-black text-white text-xl py-4">
-            <NavLink to="/" className="hover:underline">Home</NavLink>
-            <NavLink to="/about" className="hover:underline">About</NavLink>
-            <NavLink to="/profile" className="hover:underline">Profile</NavLink>
+            <NavLink to="/" className={({ isActive }) => isActive ? "hover:underline underline" : "hover:underline"}>
+                Home
+            </NavLink>
+            <NavLink to="/about" className={({ isActive }) => isActive ? "hover:underline underline" : "hover:underline"}>
+                About
+            </NavLink>
+            <NavLink to="/profile" className={({ isActive }) => isActive ? "hover:underline underline" : "hover:underline"}>
+                Profile
+            </NavLink>
         </nav>
     )
 }
