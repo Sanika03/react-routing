2725ffd3c292ad48355348c25168abe1daa4edc7
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-09 21:09:23 +0530

    Add 'Company' page

---
+++ src/pages/Company.jsx
@@ -0,0 +1,9 @@
+export const Company = () => {
+    return (
+        <div>
+            <h2 className="text-center py-20 text-black text-4xl font-bold">
+                This is Company Page
+            </h2>
+        </div>
+    )
+}
