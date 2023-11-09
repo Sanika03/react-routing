b766db284f1863ca5c568c7bd71f81a005bc74d2
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-09 21:37:50 +0530

    Update company and about route path to include id

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,8 +7,8 @@ export const About = () => {
         <div>
             <NavBar/>
             <div className="text-3xl font-bold underline flex gap-10 justify-center py-4">
-                <NavLink to="/about/team" className="hover:opacity-50">Team</NavLink>
-                <NavLink to="/about/company" className="hover:opacity-50">Company</NavLink>
+                <NavLink to="/about/team/1" className="hover:opacity-50">Team</NavLink>
+                <NavLink to="/about/company/1" className="hover:opacity-50">Company</NavLink>
             </div>
             <div>
                 <Outlet />
