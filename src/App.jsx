43b2e407e95df2bd97d8a85d67e443ee0d37d107
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-09 18:23:47 +0530

    Add routes for home, about and profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Profile } from "./pages/Profile";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/> 
+  },
+  {
+    path: "/about",
+    element: <About/> 
+  },
+  {
+    path: "/profile",
+    element: <Profile/> 
+  }
+])
 
 function App() {
   return (
     <>
+      <RouterProvider router={router}/>
     </>
   )
 }
