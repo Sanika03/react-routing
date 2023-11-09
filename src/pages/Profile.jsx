38d275cc1c378373eec0e2f9425495590d13231c
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-09 18:23:23 +0530

    Add 'Profile' page

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import { NavBar } from "../components/NavBar"
+
+export const Profile = () => {
+    return (
+        <div>
+            <NavBar/>
+            <h2 className="text-center py-20 text-black text-4xl font-bold">
+                This is Profile Page
+            </h2>
+        </div>
+    )
+}
