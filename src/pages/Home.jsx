6c2bcd444d36c08dcb74d370b96dce3efb7c7c88
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-09 18:23:15 +0530

    Add 'Home' page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { NavBar } from "../components/NavBar"
+
+export const Home = () => {
+    return (
+        <div>
+            <NavBar/>
+            <h2 className="text-center py-20 text-black text-4xl font-bold">
+                This is Home Page
+            </h2>
+        </div>
+    )
+}
